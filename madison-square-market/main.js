9f163037ecd57a9832d82cd9c87e016a0a99945b
yiyun_tan@selinc.com <Yiyun_Tan@selinc.com>
2018-11-01 11:16:38 -0700

    add jquery menu dropdown

---
+++ madison-square-market/main.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+  $('#cart').on('click', () => {
+    $('#cartMenu').show();
+  });
+  $('#account').on('click', () => {
+    $('#accountMenu').show();
+  });
+  $('#help').on('click', () => {
+    $('#helpMenu').show();
+  });
+  
+  $('#cart').on('mouseleave', () => {
+    $('#cartMenu').hide();
+  });
+  $('#account').on('mouseleave', () => {
+    $('#accountMenu').hide();
+  });
+  $('#help').on('mouseleave', () => {
+    $('#helpMenu').hide();
+  });
+});
